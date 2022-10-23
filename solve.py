6c91d7bb2b418ca3a3bedb104206effd7c85e548
ToTranSonBa <84238706+ToTranSonBa@users.noreply.github.com>
2022-10-23 15:07:20 +0700

    file txt

--- solve.py
+++ solve.py
@@ -24,6 +24,9 @@ def solve(input):
 				handle.y_index -= 10
 				x_size = handle.x_index*col
 				y_size = handle.x_index*row
+
+			# bfs
+			totalcost = 0
 			DISPLAYSURF = pygame.display.set_mode((x_size, y_size))
 			DISPLAYSURF.fill((255, 255, 255))
 			visited = [[False for i in range(col)] for j in range(row)]
@@ -34,6 +37,16 @@ def solve(input):
 			bfs.bfs(l, bonus_points, start, end, visited, route, route2)
 			handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
 			pygame.image.save(DISPLAYSURF, os.path.join(pathOutput,"bfs.jpg"))
+			f = open(pathOutput + "/bfs.txt", "w+")
+			string = ""
+			for i in route:
+				string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+				totalcost += 1
+			f.write(string)
+			f.write("\nchi phi: " + str(totalcost))
+			f.close()
+			# dfs
+			totalcost = 0
 			visited = [[False for i in range(col)] for j in range(row)]
 			route = []
 			route2 = []
@@ -41,6 +54,16 @@ def solve(input):
 			dfs.dfs(l, bonus_points, start, end, visited, route, route2)
 			handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
 			pygame.image.save(DISPLAYSURF, os.path.join(pathOutput, "dfs.jpg"))
+			f = open(pathOutput + "/dfs.txt", "w+")
+			string = ""
+			for i in route:
+				string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+				totalcost += 1
+			f.write(string)
+			f.write("\nchi phi: " + str(totalcost))
+			f.close()
+			# ucs
+			totalcost = 0
 			visited = [[False for i in range(col)] for j in range(row)]
 			route = []
 			route2 = []
@@ -48,8 +71,16 @@ def solve(input):
 			UCS_Test.ucs(l, bonus_points, start, end, visited, route, route2)
 			handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
 			pygame.image.save(DISPLAYSURF, os.path.join(pathOutput, "ucs.jpg"))
-
+			f = open(pathOutput + "/ucs.txt", "w+")
+			string = ""
+			for i in route:
+				string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+				totalcost += 1
+			f.write(string)
+			f.write("\nchi phi: " + str(totalcost))
+			f.close()
 			# GBFS
+			totalcost = 0
 			visited = [[False for i in range(col)] for j in range(row)]
 			route = []
 			route2 = []
@@ -57,7 +88,16 @@ def solve(input):
 			GBFS_test.gbfs(l, bonus_points, start, end, visited, route, route2)
 			handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
 			pygame.image.save(DISPLAYSURF, os.path.join(pathOutput, "gbfs.jpg"))
+			f = open(pathOutput + "/gbfs.txt", "w+")
+			string = ""
+			for i in route:
+				string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+				totalcost += 1
+			f.write(string)
+			f.write("\nchi phi: " + str(totalcost))
+			f.close()
 			# astar
+			totalcost = 0
 			visited = [[False for i in range(col)] for j in range(row)]
 			route = []
 			route2 = []
@@ -65,6 +105,14 @@ def solve(input):
 			Astar_test.Astar(l, bonus_points, start, end, visited, route, route2)
 			handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
 			pygame.image.save(DISPLAYSURF, os.path.join(pathOutput, "astar.jpg"))
+			f = open(pathOutput + "/astar.txt", "w+")
+			string = ""
+			for i in route:
+				string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+				totalcost += 1
+			f.write(string)
+			f.write("\nchi phi: " + str(totalcost))
+			f.close()
 	if input == "advance":
 		pathInput = 'Input/advance'
 		pathfile = os.path.join(pathInput, 'Input1.txt')
@@ -105,6 +153,7 @@ def solve(input):
 			y_size = handle.x_index*row
 		DISPLAYSURF = pygame.display.set_mode((x_size, y_size))
 		DISPLAYSURF.fill((255, 255, 255))
+		totalcost = 0
 		visited = [[False for i in range(col)] for j in range(row)]
 		route = []
 		route2 = []
@@ -114,4 +163,9 @@ def solve(input):
 		#dfs.dfs(l, bonus_points, start, end, visited, route, route2)
 		#bfs.bfs(l, bonus_points, start, end, visited, route, route2)
 		handle.test_init(matrix, bonus_points, route2, route, DISPLAYSURF, end, start, delay)
-		
+		string = ""
+		for i in route:
+			string = string + '(' + str(i[0]) + ', ' + str(i[1]) + '), ' 
+		print(route)
+		print(string)
+		print(totalcost)
